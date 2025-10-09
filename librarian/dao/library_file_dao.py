0cc2048ebc86dc3933b617a24d1898101a8406d2
seabird64 <seabird64@email.not.exit.io>
2025-10-09 11:10:19 +0100

    Add find method to LibraryFileDao for flexible querying by hash prefix or filename

--- librarian/dao/library_file_dao.py
+++ librarian/dao/library_file_dao.py
@@ -6,3 +6,16 @@ class LibraryFileDao(BaseDao):
     def exist(self, hash_id: str) -> bool:
         clause = LibraryFile.hash_id == hash_id
         return self.exists(clause)
+
+    def find(self, hash_prefix: str = None, filename: str = None) -> list[LibraryFile]:
+        """Find library files with optional filtering by hash prefix or filename."""
+        with self.create_session() as session:
+            query = session.query(LibraryFile)
+
+            if hash_prefix is not None:
+                query = query.filter(LibraryFile.hash_id.startswith(hash_prefix))
+
+            if filename is not None:
+                query = query.filter(LibraryFile.file_name.like(f"%{filename}%"))
+
+            return query.all()
